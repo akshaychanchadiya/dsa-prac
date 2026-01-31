350f1d92b6735742d908de3f5bb95d319f53d4d5
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-01-31 18:55:01 +0530

    my first commit

---
+++ src/main/java/Practise/primeNumber.java
@@ -0,0 +1,26 @@
+package Practise;
+
+public class primeNumber {
+
+    //Check if a Number is Prime
+
+    public static void main(String[] args) {
+
+        int n = 79;
+        System.out.println(isPrime(n));
+
+    }
+
+    public static Boolean isPrime(int n) {
+        if (n<=1) {
+            return false;
+        }
+        for(int i=2; i*i<=n; i++) {
+
+            if (n%i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
