3c8bb537b6370db8e89f1f9cc0a6226f50d377e9
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-02-04 21:48:10 +0530

    changed_primeNumber_code

--- src/main/java/Practise/PrimeNumber.java
+++ src/main/java/Practise/PrimeNumber.java
@@ -14,9 +14,8 @@ public class PrimeNumber {
         if (n<=1) {
             return false;
         }
-        for(int i=2; i*i<=n; i++) {
-
-            if (n%i == 0) {
+        for(int i=2; i<n-1 ; i++) {
+            if(n%i==0) {
                 return false;
             }
         }
