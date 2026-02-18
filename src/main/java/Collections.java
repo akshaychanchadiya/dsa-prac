1b29eba7e21afd7fe80206678809fa1a5b6550c7
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-02-18 22:53:53 +0530

    18022026

---
+++ src/main/java/Collections.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Collections {
+
+    public static void main(String[] args) { // entry point
+        // CRUD in arraylist
+        ArrayList<String> a1 = new ArrayList<>();
+        a1.add("a");
+        a1.add("b");
+        a1.add("c");
+
+        System.out.println(a1);
+        System.out.println(a1.get(1));
+
+        a1.set(1, "d");
+        System.out.println(a1);
+
+        a1.remove(1);
+        System.out.println(a1);
+
+        //CRUD in sets
+        HashSet<String> s1 = new HashSet<>();
+        s1.add("A");   //CREATE
+        s1.add("B");
+        s1.add("C");
+
+        System.out.println(s1); //READ ENTIRE SET
+        System.out.println(s1.contains("A")); //READ ONE VALUE
+
+        //UPDATE: DELETE AND ADD
+        s1.remove("B");
+        s1.add("D");
+        System.out.println(s1);
+
+        s1.remove("D"); //DELETE
+        System.out.println(s1);
+
+
+        //CRUD in maps
+        HashMap<Integer, String> m1 = new HashMap<>();
+        m1.put(1, "A"); //CREATE
+        m1.put(2, "B");
+        m1.put(3, "C");
+
+
+        System.out.println(m1);
+        System.out.println(m1.get(2));
+
+        m1.put(2, "D"); //UPDATE
+        System.out.println(m1);
+
+    }
+}
---
+++ src/main/java/Vowels.java
@@ -0,0 +1,17 @@
+public class Vowels {
+
+    public static void main (String[] args) {
+        String s = "Akshay";
+        int vowelCount = 0;
+        char[] c = s.toLowerCase().toCharArray();
+
+        for (int i=0;i<c.length;i++) {
+            if (c[i] == 'a'|| c[i] == 'e'|| c[i] == 'i'|| c[i] == 'o'|| c[i] == 'u') {
+
+                vowelCount++;
+            }
+        }
+        System.out.println(vowelCount);
+
+    }
+}
