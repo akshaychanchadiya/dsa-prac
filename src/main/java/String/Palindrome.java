c698bec6b917cd5199eb1de9e1922fba5a399c5d
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-02-21 18:41:06 +0530

    21022026

--- src/main/java/String/Palindrome.java
+++ src/main/java/String/Palindrome.java
@@ -1,5 +1,23 @@
-package String;
+    package String;
 
-public class Palindrome {
+    public class Palindrome {
 
-}
+        public static void main (String[] args) {
+            String s = "madam";
+
+            String s1 = "";
+
+            char[] ch = s.toCharArray();
+            for(int i=ch.length-1;i>=0;i--) {
+
+                s1 += ch[i];
+            }
+
+            if (s.equals(s1)) {
+                System.out.println("given string is Palindrome");
+            }
+            else {
+                System.out.println("given string is not Palindrome");
+            }
+        }
+    }
---
+++ src/main/java/String/Reverse.java
@@ -0,0 +1,36 @@
+package String;
+
+import com.sun.jna.StringArray;
+
+import java.util.Arrays;
+
+public class Reverse {
+
+    public static void main (String[] args) {
+
+        String s = "Hello World";
+        String result = "";
+
+        String[] a = s.split(" ");
+
+        for (int j=0;j<a.length;j++) {
+
+            String w1 = a[j];
+            String r = "";
+
+            for(int i=w1.length()-1;i>=0;i--) {
+
+                r += w1.charAt(i);
+            }
+
+            result += r;
+
+            if (j == 0){
+                result += " ";
+            }
+        }
+        System.out.println(result);
+
+
+    }
+}
