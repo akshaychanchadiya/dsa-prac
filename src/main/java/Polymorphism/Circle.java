82a301d68e293ba913ee7e3e0c847d687d46d929
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-03-25 23:12:30 +0530

    25032026

---
+++ src/main/java/Polymorphism/Circle.java
@@ -0,0 +1,15 @@
+package Polymorphism;
+
+public class Circle extends Shape{
+
+    private double radius;
+
+    Circle(String name,double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI*radius*radius;
+    }
+}
