e95e00432dc6eaeb837defd9d0e9e45e5551740b
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-03-27 21:55:45 +0530

    27032026

---
+++ src/main/java/Polymorphism/Rectangle.java
@@ -0,0 +1,16 @@
+package Polymorphism;
+
+public class Rectangle extends Shape{
+    private double length;
+    private double width;
+
+    Rectangle(String name,double length,double width) {
+        super(name);
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+}
