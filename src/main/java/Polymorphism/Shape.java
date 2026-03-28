6dbf64459ee352cd0abc96e4de29675fedbcb60b
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-03-28 16:06:25 +0530

    28032026

---
+++ src/main/java/Polymorphism/Shape.java
@@ -0,0 +1,20 @@
+package Polymorphism;
+
+public class Shape {
+
+    private String name;
+
+    Shape(String name) {
+
+        this.name = name;
+    }
+
+    public double calculateArea() {
+        return 0;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+}
