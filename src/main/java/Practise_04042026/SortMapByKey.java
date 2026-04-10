9ce1545987ff6de056e2632c055f27932d3276b4
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-04-10 21:47:41 +0530

    10042026

---
+++ src/main/java/Practise_04042026/SortMapByKey.java
@@ -0,0 +1,19 @@
+package Practise_04042026;
+
+import java.util.*;
+
+public class SortMapByKey {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer,String> map = new HashMap<>();
+
+        map.put(3,"C");map.put(1,"A");map.put(2,"B");
+
+        Map<Integer,String> sortedMap = new TreeMap<>(map);
+
+        System.out.println(sortedMap);
+
+
+    }
+}
