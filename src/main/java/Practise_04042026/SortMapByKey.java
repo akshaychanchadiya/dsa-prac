c1bec27bd2ab69e37a5a4f1a1ca6f316843aec50
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-04-19 09:34:15 +0530

    19042026

--- src/main/java/Practise_04042026/SortMapByKey.java
+++ src/main/java/Practise_04042026/SortMapByKey.java
@@ -6,8 +6,6 @@ public class SortMapByKey {
 
     public static void main(String[] args) {
 
-        //HashMap<Integer,String> map = new HashMap<>();
-
         HashMap<Integer,String> map = new HashMap<>();
 
         map.put(3,"C");map.put(1,"A");map.put(2,"B");
