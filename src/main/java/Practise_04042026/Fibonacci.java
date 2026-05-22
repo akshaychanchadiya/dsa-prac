395a4d1599a346fb4e762dbfd506372bc6a52c1a
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-05-22 09:39:59 +0530

    22052026

--- src/main/java/Practise_04042026/Fibonacci.java
+++ src/main/java/Practise_04042026/Fibonacci.java
@@ -6,7 +6,7 @@ public class Fibonacci {
         int a = 0,b=1;
 
         for (int i =1;i<=10;i++) {
-            //System.out.println(a+ " ");
+            System.out.println(a+ " ");
             int c = a+b;
             a = b;
             b = c;
