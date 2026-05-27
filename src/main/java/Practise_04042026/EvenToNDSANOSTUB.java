b052b99933f2747bc782bf4df3225ade819ad7eb
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-05-27 21:29:47 +0530

    27052026

---
+++ src/main/java/Practise_04042026/EvenToNDSANOSTUB.java
@@ -0,0 +1,19 @@
+package Practise_04042026;
+
+import java.util.Scanner;
+
+public class EvenToNDSANOSTUB {
+
+     public static void main(String[] args) {
+
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+
+         for(int i = 1; i <= n; i++) {
+
+             if (i%2==0) {
+                 System.out.println(i);
+             }
+         }
+    }
+}
