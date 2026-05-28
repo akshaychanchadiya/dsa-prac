ad7a3c681dae3dfb2046a64bfa9120340c14864b
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-05-28 23:41:54 +0530

    28052026

---
+++ src/main/java/Practise_04042026/SubstringDSANOSTUB.java
@@ -0,0 +1,23 @@
+package Practise_04042026;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class SubstringDSANOSTUB {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        HashSet<String> set = new HashSet<>();
+
+        for(int i=0;i<s.length();i++) {
+            for(int j=i+1;j<=s.length();j++) {
+                set.add(s.substring(i,j));
+            }
+        }
+
+        System.out.println(set.size());
+    }
+}
