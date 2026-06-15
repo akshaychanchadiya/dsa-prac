14395caaa62866aa444c7bfc710752848c836a78
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-06-15 20:31:52 +0530

    15062026

---
+++ src/main/java/Practise_04042026/Capitalise_2.java
@@ -0,0 +1,32 @@
+package Practise_04042026;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Capitalise_2 {
+    public static void main(String[] args) {
+        String a = "hello akshay! how are you?";
+
+        String[] words = a.split(" ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i< words.length;i++) {
+            String word = words[i];
+
+            for (int j =0 ; j< word.length();j++) {
+                if(j==0) {
+                   String first = String.valueOf(word.charAt(j));
+
+                   sb.append(first.toUpperCase());
+                }
+                else {
+                    sb.append(word.charAt(j));
+                }
+            }
+            sb.append(" ");
+        }
+        System.out.println(sb);
+
+    }
+}
