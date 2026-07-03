6aeceb3ffde9955de4754556b26a9aa77898aa25
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-07-03 22:09:37 +0530

    03072026

--- src/main/java/Practise_04042026/Rotate_byK.java
+++ src/main/java/Practise_04042026/Rotate_byK.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Rotate_byK {
 
     public static void main(String[] args) {
-        //int[] a = {5,8,9,3,6};
+        int[] a = {5,8,9,3,6};
 
         int k = 3;
 
