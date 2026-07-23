66bdc5d126a5d0631e58fae9927b7bb74fd5b139
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-07-23 19:48:24 +0530

    23072026

---
+++ src/main/java/Practise_04042026/EqualVowelsDSANOSTUB.java
@@ -0,0 +1,25 @@
+package Practise_04042026;
+
+import java.util.Scanner;
+
+public class EqualVowelsDSANOSTUB {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for (char c : a.toCharArray()) {
+            if (c == 'a'|| c == 'e'|| c == 'i'|| c == 'o' || c == 'u') {
+                vowels++;
+            }
+            else {
+                consonants++;
+            }
+        }
+
+        System.out.println(consonants-vowels);
+    }
+}
