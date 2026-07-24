915f8bd5fb5f67749ae999bce0897e5155a38ea3
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-07-24 22:07:43 +0530

    24072026

---
+++ src/main/java/Practise_04042026/ReverseStringDSANOSTUB.java
@@ -0,0 +1,17 @@
+package Practise_04042026;
+
+import java.util.Scanner;
+
+public class ReverseStringDSANOSTUB {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(a);
+        String reverse = sb.reverse().toString();
+
+        System.out.println(reverse);
+
+    }
+}
