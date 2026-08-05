f2863ac8cd7974f9c7a1d51789f89a340d4d7fad
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-08-05 21:35:10 +0530

    05082026

--- src/main/java/Practise_04042026/EqualVowelsDSANOSTUB.java
+++ src/main/java/Practise_04042026/EqualVowelsDSANOSTUB.java
@@ -16,7 +16,7 @@ public class EqualVowelsDSANOSTUB {
                 vowels++;
             }
             else {
-                consonants++;
+               // consonants++;
             }
         }
 
