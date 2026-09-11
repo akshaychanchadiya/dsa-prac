33c9b622b831ba54c27bb9c574a905a36cdbb2c5
akshaychanchadiya <akshaychanchadiya@gmail.com>
2026-09-11 21:21:36 +0530

    11092026

--- src/main/java/Mix/UniqueCharacterSet.java
+++ src/main/java/Mix/UniqueCharacterSet.java
@@ -7,7 +7,7 @@ public class UniqueCharacterSet {
     public static void main (String[] args) {
         String a = "banana";
 
-        HashSet<Character> s = new HashSet<>();
+        //HashSet<Character> s = new HashSet<>();
 
         for (char d : a.toCharArray()) {
 
